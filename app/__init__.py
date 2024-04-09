4e0bbac85c51f41de947651ab69aa05666c46aca
andrii-peliak-wmg <vseprav@gmail.com>
2024-04-09 18:27:52 +0300

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+
